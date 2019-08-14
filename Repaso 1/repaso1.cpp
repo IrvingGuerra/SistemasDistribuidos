a4d8f4fa260f0d8afdc4fa19bacd9b5f20a2a022
IrvingGuerra <guerravargasirving@gmail.com>
2019-08-14 08:24:32 -0500

    REspuestas hasta la 4

--- Repaso 1/repaso1.cpp
+++ Repaso 1/repaso1.cpp
@@ -2,14 +2,46 @@
 #include<stdio.h>
 using namespace std;
 
-char caracter = 0;
-int entero = 0;
-float flotante = 0;
-long largo = 0;
-double doble = 0;
+int main(void){
 
-int main(){
+	char caracter = 0;
+	int entero = 0;
+	float flotante = 0;
+	long largo = 0;
+	double doble = 0;
 
-	printf("%p\n", caracter);
-	
+
+
+	printf("0x%p\n", &caracter);
+	printf("0x%p\n", &entero);
+	printf("0x%p\n", &flotante);
+	printf("0x%p\n", &largo);
+	printf("0x%p\n", &doble);
+
+	printf("%d\n", sizeof(caracter));
+	printf("%d\n", sizeof(entero));
+	printf("%d\n", sizeof(flotante));
+	printf("%d\n", sizeof(largo));
+	printf("%d\n", sizeof(doble));
+
+
+
+	return 0;
 }
+
+
+//CUantos bits se almacenan en una direccion de mmoria? 8 bits / 1 byte
+//0xFF, 255, 11111111
+//WIndows NO, LInux SI
+//Windows NO, Linux SI
+//Si
+
+
+//512MB En esta PC
+//536,870,912 bytes // 512MB
+
+
+//4.- Ocupan lo mismo, dependiendo la arcquitectura, en este caso 4 para 32 y 8 para 64
+
+//
+
