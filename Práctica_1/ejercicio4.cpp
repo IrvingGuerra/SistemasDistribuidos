9e915851fe730be81448ad603d43ab6d03e48a9f
Alan Fuentes <fuentes.alan.98@gmail.com>
2019-08-16 07:49:44 -0500

    Dos cast en E4

--- Práctica_1/ejercicio4.cpp
+++ Práctica_1/ejercicio4.cpp
@@ -5,6 +5,8 @@ int main(int argc, char const *argv[])
 {
     double c = 20;
     double f;
+    f = (double)(9)/5 * c + 32.0;
+    cout << f << endl;
     f = static_cast<double>(9)/5 * c + 32.0;
     cout << f << endl;
     return 0;
