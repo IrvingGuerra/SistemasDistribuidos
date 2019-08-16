928f298497156adb89a129fe72c45ab092f42aeb
Vázquez Núñez Erick <erick.f.nunez@gmail.com>
2019-08-16 07:52:57 -0500

    Add files via upload

---
+++ Práctica_1/programa1_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main( )
+{
+string nombre; //Variable para almacenar el nombre
+
+cout << "Cuál es tu nombre?" << endl;
+cin >> nombre;
+cout << "Hola " << nombre << endl;
+
+return 0;
+}
