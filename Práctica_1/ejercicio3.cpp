a901ef5a8db196c6d1b07b62376849c11d6281d4
Vázquez Núñez Erick <erick.f.nunez@gmail.com>
2019-08-16 07:46:39 -0500

    Add files via upload

---
+++ Práctica_1/ejercicio3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+	double c, f;
+	cout << "ingrese los grados centigrados a convertir" << endl;
+	cin >> c;
+	f = static_cast<double>(static_cast<double>(9)/static_cast<double>(5)) * c + 32.0;
+	cout << "el  equivalente en grados fahrenheit es:" << f << endl;
+	return 0;
+}
