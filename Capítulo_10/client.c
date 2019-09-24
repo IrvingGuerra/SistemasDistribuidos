f5beabbb02ce4564bce27e8eaec24bfa188938b6
Alan Fuentes <fuentes.alan.98@gmail.com>
2019-09-24 08:16:53 -0500

    client con mayor valor de len

--- Capítulo_10/client.c
+++ Capítulo_10/client.c
@@ -6,18 +6,20 @@
 #include <strings.h>
 #include <arpa/inet.h>
 
+#define N 16376
+
 int puerto = 7200;
 
-int main(void)
+int main(int argc, char **argv)
 {
    struct sockaddr_in msg_to_server_addr, client_addr;
-   int s, num[2], res;
+   int s, res;
 
    s = socket(AF_INET, SOCK_DGRAM, 0);
    /* rellena la dirección del servidor */
    bzero((char *)&msg_to_server_addr, sizeof(msg_to_server_addr));
    msg_to_server_addr.sin_family = AF_INET;
-   msg_to_server_addr.sin_addr.s_addr = inet_addr("192.168.122.1");
+   msg_to_server_addr.sin_addr.s_addr = inet_addr("10.100.74.168");
    msg_to_server_addr.sin_port = htons(puerto);
 
    /* rellena la direcciòn del cliente*/
@@ -26,11 +28,14 @@ int main(void)
    client_addr.sin_addr.s_addr = INADDR_ANY;
 
    /*cuando se utiliza por numero de puerto el 0, el sistema se encarga de asignarle uno */
-   client_addr.sin_port = htons(0);
+   client_addr.sin_port = htons(6666);
    bind(s, (struct sockaddr *)&client_addr, sizeof(client_addr));
-   num[0] = 2;
-   num[1] = 5; /*rellena el mensaje */
-   sendto(s, (char *)num, 2 * sizeof(int), 0, (struct sockaddr *)&msg_to_server_addr, sizeof(msg_to_server_addr));
+   //num[0] = 2;
+   //num[1] = 5; /*rellena el mensaje */
+   int num[N];
+   num[N - 2] = 10;
+   num[N - 1] = 20;
+   sendto(s, (char *)num, N * sizeof(int), 0, (struct sockaddr *)&msg_to_server_addr, sizeof(msg_to_server_addr));
 
    //char * s_addr_str = mem;
    uint32_t s_addr_server = msg_to_server_addr.sin_addr.s_addr;
@@ -44,11 +49,9 @@ int main(void)
           s_addr_server >> 8 & 0xff,
           s_addr_server & 0xff,
           s_port_server);
+
    /* se bloquea esperando respuesta */
    recvfrom(s, (char *)&res, sizeof(int), 0, NULL, NULL);
-
-   //printf();
-
-   printf("2 + 5 = %d\n", res);
+   printf("suma = %d\n", res);
    close(s);
 }
