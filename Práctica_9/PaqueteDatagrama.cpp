d35a35f9e5e4c890dd1f50ab6165dd6e3a464b1a
erickvazquez <erick.f.nunez@gmail.com>
2019-09-25 08:24:23 -0500

    paquete dtagrama h y cpp

--- Práctica_9/PaqueteDatagrama.cpp
+++ Práctica_9/PaqueteDatagrama.cpp
@@ -7,11 +7,11 @@ using namespace std;
 //Fecha::Fecha(int dd, int mm, int aaaa) : dia(dd), mes(mm), anio(aaaa){}
 
 
-void PaqueteDatagrama::PaqueteDatagrama(char *, unsigned int, char *, int ){
+void PaqueteDatagrama::PaqueteDatagrama(char *data, unsigned int longitud, char *ip, int puerto){
 
 }
 
-void PaqueteDatagrama::PaqueteDatagrama(unsigned int ){
+void PaqueteDatagrama::PaqueteDatagrama(unsigned int longitud){
 
 }
 
@@ -20,23 +20,23 @@ void PaqueteDatagrama::~PaqueteDatagrama(){
 }
 
 char *PaqueteDatagrama::obtieneDireccion(){
-
+	return *ip;
 }
 unsigned int PaqueteDatagrama::obtieneLongitud(){
-
+	return longitud;
 }
 int PaqueteDatagrama::obtienePuerto(){
-
+	return puerto;
 }
 char *PaqueteDatagrama::obtieneDatos(){
-
+	return *datos;
 }
-void PaqueteDatagrama::inicializaPuerto(int){
-
+void PaqueteDatagrama::inicializaPuerto(int pto){
+	puerto = pto;
 }
-void PaqueteDatagrama::inicializaIp(char *){
-
+void PaqueteDatagrama::inicializaIp(char *ipadd){
+	*ip = *ipadd;
 }
-void PaqueteDatagrama::inicializaDatos(char *){
-	
+void PaqueteDatagrama::inicializaDatos(char *data){
+	*datos = *data;
 }
