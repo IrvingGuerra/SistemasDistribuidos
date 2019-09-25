0c50c38bdf41fea98fd2d47d3a05d363cc5e1f68
erickvazquez <erick.f.nunez@gmail.com>
2019-09-25 08:18:41 -0500

    paquete dtagrama h y cpp

---
+++ Práctica_9/PaqueteDatagrama.cpp
@@ -0,0 +1,42 @@
+#include "PaqueteDatagrama.h"
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+//Fecha::Fecha(int dd, int mm, int aaaa) : dia(dd), mes(mm), anio(aaaa){}
+
+
+void PaqueteDatagrama::PaqueteDatagrama(char *, unsigned int, char *, int ){
+
+}
+
+void PaqueteDatagrama::PaqueteDatagrama(unsigned int ){
+
+}
+
+void PaqueteDatagrama::~PaqueteDatagrama(){
+
+}
+
+char *PaqueteDatagrama::obtieneDireccion(){
+
+}
+unsigned int PaqueteDatagrama::obtieneLongitud(){
+
+}
+int PaqueteDatagrama::obtienePuerto(){
+
+}
+char *PaqueteDatagrama::obtieneDatos(){
+
+}
+void PaqueteDatagrama::inicializaPuerto(int){
+
+}
+void PaqueteDatagrama::inicializaIp(char *){
+
+}
+void PaqueteDatagrama::inicializaDatos(char *){
+	
+}
