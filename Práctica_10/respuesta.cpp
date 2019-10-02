b48e5c54eec55a2465a0fd0ff3403140593839a3
Irving <guerravargasirving@gmail.com>
2019-10-02 08:08:08 -0500

    resp.h

---
+++ Práctica_10/respuesta.cpp
@@ -0,0 +1,14 @@
+#include "respuesta.h"
+
+respuesta::Respuesta(int pl){ //recibe puerto
+	SocketDatagrama sd(pl);
+}
+
+
+mensaje respuesta::getRequest(){
+
+}
+
+respuesta::sendReply(char *respuesta){
+
+}
